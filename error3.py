8092db83f1d80daf9b6b2e79a3208d41fcac1788
hivaibhav123 <109505303+hivaibhav123@users.noreply.github.com>
2022-07-22 12:31:52 +0530

    Update error3.py

--- error3.py
+++ error3.py
@@ -1,3 +1,9 @@
 x = -646512
 if x < 0:
   raise Exception("Sorry, no numbers below zero")
+  
+  #output
+  #Traceback (most recent call last):
+  #File "<string>", line 3, in <module>
+#Exception: Sorry, no numbers below zero
+#> 
